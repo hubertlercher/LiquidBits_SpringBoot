5ef848f626ff76ab6940d004526cf01fe72f144f
Hubert Lercher (4AAIF) <hubert.lercher@edu.htl-klu.at>
2024-01-23 12:30:44 +0100

    Email Controller -> JSON

--- src/main/java/com/example/liquidbits_springboot/restcontroller/EmailController.java
+++ src/main/java/com/example/liquidbits_springboot/restcontroller/EmailController.java
@@ -1,10 +1,16 @@
 package com.example.liquidbits_springboot.restcontroller;
 
+import com.example.liquidbits_springboot.model.Drink;
+import com.example.liquidbits_springboot.model.Email;
 import com.example.liquidbits_springboot.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class EmailController {
@@ -13,8 +19,9 @@ public class EmailController {
     private EmailService emailService;
 
     @PostMapping("/send-email")
-    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
-        emailService.sendEmail(to, subject, text);
+    public String sendEmail(@Valid @RequestBody Email email, BindingResult bindingResult) {
+        emailService.sendEmail(email.getTo(), email.getSubject(), email.getText());
         return "E-Mail erfolgreich gesendet!";
     }
+
 }
