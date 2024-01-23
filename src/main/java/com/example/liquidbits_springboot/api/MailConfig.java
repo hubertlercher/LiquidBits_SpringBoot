bcf1687daf8f374cc54d4674b6c7d178f33af592
Hubert Lercher (4AAIF) <hubert.lercher@edu.htl-klu.at>
2024-01-23 13:04:32 +0100

    Mail Config Klasse

---
+++ src/main/java/com/example/liquidbits_springboot/api/MailConfig.java
@@ -0,0 +1,27 @@
+package com.example.liquidbits_springboot.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("hubert.lercher1@gmail.com");
+        mailSender.setPassword("dovxir-cahroj-takwU0");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
