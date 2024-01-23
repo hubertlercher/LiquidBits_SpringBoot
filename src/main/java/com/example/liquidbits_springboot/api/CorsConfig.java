888fbbc48409716fe4b112dec91d7fed8e428a26
Hubert Lercher (4AAIF) <hubert.lercher@edu.htl-klu.at>
2024-01-23 12:44:31 +0100

    CorsConfig Anpassungen

--- src/main/java/com/example/liquidbits_springboot/api/CorsConfig.java
+++ src/main/java/com/example/liquidbits_springboot/api/CorsConfig.java
@@ -1,8 +1,13 @@
 package com.example.liquidbits_springboot.api;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
